3bfb2a1bec15840201415b958fd0f55a8f828f5b
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-08-12 11:03:35 -0700

    add new mvc

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
